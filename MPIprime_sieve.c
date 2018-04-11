208e8d515d7fd1f3dcfac76ae8227ce45fcb4886
Mario Carl <mariocarl@gmail.com>
2018-04-11 17:59:50 -0500

    Second final code

--- MPIprime_sieve.c
+++ MPIprime_sieve.c
@@ -3,9 +3,9 @@
 #include <math.h>
 #include <mpi.h>     /* For MPI functions, etc */
 
-void sieve(char *array, int n);
+int sieve(char *array, int n);
 void print(char *array, int n, int begin);
-void sieve_two(char *primes, char *array, int start, int end);
+int sieve_two(char *primes, char *array, int start, int end);
 /* main */
 int main(void) {
     int my_rank,
@@ -17,8 +17,9 @@ int main(void) {
     local_n,
     extra_n,
     n_start,
-    n_end;
-    
+    n_end,
+    local_check;
+    double start_time, stop_time, ticks_start, ticks_stop;
     
     /* Start up MPI */
     MPI_Init(NULL, NULL);
@@ -29,15 +30,21 @@ int main(void) {
     /* Get my rank among all the processes */
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     
+    //user input
     if(my_rank == 0){
         printf("Enter #n \n");
-        scanf("%ld",&n);
+        scanf("%ld", &n);
         // no longer set p, MPI_GATHERV uses comm_sz to determin communication.
     }
     
     //Make sure all processors get input values
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
     
-    MPI_Bcast(&n,1,MPI_INT,0,MPI_COMM_WORLD);
+    //start timer
+    if(my_rank==0){
+        //ticks_start = MPI_Wtick();
+        start_time = MPI_Wtime();
+    }
     
     //input protection
     if ( p > n || n <= 0){
@@ -52,38 +59,43 @@ int main(void) {
     int i, j;
     // work arrays
     char *sp_array, *local_array, *final_array;
-    // for MPI_Gatherv
+    
+    // for MPI_Gatherv requirments displacments & receiving counts
     int *displs, *rcounts;
     
     // get n starter primes
-    n_start_primes = ( int)(ceil(sqrt(n)));
-    
-    
+    n_start_primes = (int)(ceil(sqrt(n)));
     
     // divide the work.
     local_n = (n - n_start_primes)/p;
+    
     // have any extras?
     extra_n = (n - n_start_primes)%p;
+    
     //keep track of largest possible local_n
     int largest_n = local_n + 1;
-    //pass extra to nodes (extra_n < p) always!
+    
+    //pass extra to to local_n; (extra_n < p) always!
     if(my_rank < extra_n)
     {
-        local_n = local_n+1;
+        ++local_n;
     }
-    // start and end point for each process {(largest_n - local_n) == 1 || 0} always!
-    n_start = n_start_primes + my_rank*local_n + extra_n*(largest_n - local_n);
-    n_end     = n_start + local_n - 1;
-    // do work
+    // start and end points for each process {(largest_n - local_n) == 1 || 0} always!
+    n_start = n_start_primes + my_rank * local_n + extra_n * (largest_n - local_n);
+    n_end     = n_start + local_n;
+    
+    /*----------------------------DO WORK-------------------------------------*/
     if(my_rank == 0){
         //p0 will do work all at once
         //combine starting primes with local_n
         local_n+=n_start_primes;
+        
         //allocate and initailize array to 0
-        local_array = malloc((local_n) * sizeof(char));
+        local_array = malloc(local_n * sizeof(char));
         for(i = 0; i < local_n; i++){local_array[i]=0;}
+        
         // GO!
-        sieve(local_array, local_n);
+        local_check = sieve(local_array, local_n);
         
     } else {
         
@@ -94,35 +106,58 @@ int main(void) {
         //compute starter primes
         sieve(sp_array, n_start_primes);
         
-        // allocate local array initailize to 0
+        // allocate main array initailize to 0
         local_array = malloc(local_n * sizeof(char));
         for(i = 0; i < local_n; i++){local_array[i]=0;}
         
-        //compute remaining
-        sieve_two(sp_array, local_array, n_start, n_end);
+        //compute primes in local array
+        local_check = sieve_two(sp_array, local_array, n_start, n_end);
         
     }
-    // to hold number of local_n (needed for MPI_Gatherv)
-    rcounts = malloc(p*sizeof(int));
+    // put local_n values in an array (needed for MPI_Gatherv)
+    rcounts = malloc(p * sizeof(int));
+    
     // Each process tells the root how many elements it holds
     MPI_Gather(&local_n, 1, MPI_INT, rcounts, 1, MPI_INT, 0, MPI_COMM_WORLD);
     
-    // Displacements in the receive buffer for MPI_GATHERV
-    displs = malloc(p*sizeof(int));
-    // Displacement for the first chunk of data == 0
+    // Displacements for MPI_GATHERV, where to place local_array in final array;
+    displs = malloc(p * sizeof(int));
+    
+    // Calculate Displacement; my_rank(0) == 0
     for ( i = 0; i < p; i++)
-    displs[i] = (i > 0) ? (displs[i-1] + rcounts[i-1]) : 0;
+        displs[i] = (i > 0) ? (displs[i-1] + rcounts[i-1]) : 0;
+    
     // Place to hold the gathered data
     final_array = NULL;
+    
     // Allocate at root only
     if (my_rank == 0)
-    final_array = malloc((displs[p-1]+rcounts[p-1])*sizeof(char));
-    // Collect everything into the root
-    MPI_Gatherv(local_array, local_n, MPI_CHAR, final_array, rcounts, displs, MPI_CHAR, 0, MPI_COMM_WORLD);
-    //print result
-    if (my_rank == 0)
-    print(final_array,n,0);
+        final_array = malloc((displs[p-1]+rcounts[p-1])*sizeof(char));
     
+    // Gather using displacments and recv counts
+    MPI_Gatherv(local_array, local_n, MPI_CHAR, final_array, rcounts, displs, MPI_CHAR, 0, MPI_COMM_WORLD);
+    
+    //calculate total primes
+    int pcount = 0, total_prime, total_check;
+    for(i=(my_rank==0)?2:0; i < local_n; i++)
+        if(local_array[i]==0)
+            pcount++;
+    
+    //sum total primes and total checks from each process to root
+    MPI_Reduce(&pcount, &total_prime, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&local_check, &total_check, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    
+    //print results
+    if (my_rank == 0){
+        //stop time
+        stop_time = MPI_Wtime();
+        //print first 100 primes
+        print(final_array,n,0);
+        //print total and time
+        printf("Total primes: %d\n", total_prime);
+        printf("Total checks: %d\n", total_check);
+        printf("Total time: %f seconds\n", stop_time-start_time);
+    }
     
     
     MPI_Finalize();
@@ -136,79 +171,83 @@ int main(void) {
  This version of seive handles different starting points
  and uses a secondary array *primes to mark *array
  */
-void sieve_two(char *primes, char *array, int start, int end){
+int sieve_two(char *primes, char *array, int start, int end){
     int     i,
     k = 2,
-    bias = start/2,            //bias is used to get k*i in range of start
-    adjust = 0;                //adjust from even or odd
+    bias,                    //bias is used to get k*i in range of start
+    adjust = 0,                //adjust from even or odd
+    checked = 0;            //count nonprime checks
     
-    // must begin on an even number for 2.
-    if(start%2)
-    adjust=1;
+    bias = (int)ceil((double)start/(double)2);
+    
     // compute all evens first
-    for(i=bias+adjust; i*k <= end; ++i){
+    for(i=bias; i*k < end; ++i){
         array[i*k-start] = 1;
     }
     k++;
     //k>2
     
-    for(;k*k <= end;){
+    for( ; k*k < end; ){
         adjust=0;
         //get starting point
         bias=(int)ceil((double)start/(double)k);
+        if(bias<k)
+            bias = k;
         // adjust must be odd.
-        if(bias%2==0)
-        adjust++;
-        for(i=(bias)+adjust; i*k <= end;i=i+2){
+        if(bias%2 == 0)
+            adjust++;
+        for(i=(bias)+adjust; i*k < end; i=i+2){
             if(array[i*k-start]==0)
-            array[i*k-start] = 1;
+                array[i*k-start] = 1;
+            else
+                checked++;            // if number is already marked increase checked
         }
         //skip all even numbers
-        k=k+2;
+        k = k+2;
         //skip all marked odd numbers
         //use prime array to find the next k.
-        for(;primes[k]!=0;k=k+2){}
+        for( ;primes[k]!=0; k=k+2){}
     }
-    
+    return checked;
 }
 
 /*
  serial implementtion of sieve to find starting primes and p0 work
  */
-void sieve(char *array, int n){
-    int i, k = 2;
+int sieve(char *array, int n){
+    int i, k = 2, checked = 0;
     // compute all evens first
     for(i=k; i*k < n;++i){
-        if(array[i*k]==0)
         array[i*k] = 1;
     }
     k++;
     //k>2
     
-    for(;k*k < n;){
+    for( ; k*k < n; ){
         for(i=k; i*k < n;i=i+2){
             if(array[i*k]==0)
-            array[i*k] = 1;
+                array[i*k] = 1;
+            else
+                checked++;            // if number is already marked increase checked
         }
         //skip all even numbers
         k=k+2;
         //skip all marked odd numbers
         for(;array[k]!=0;k=k+2){}
     }
-    
+    return checked;
 }
 
 /* print primes */
 void print(char *array, int n, int begin){
     int i, pcount = 0;
-    for(i = 2; i < n; ++i){
+    for(i = 2; pcount < 100 && i < n; ++i){
         if (array[i]==0)
         {
             ++pcount;
-            if (pcount<100)
-            printf("%ld:  %ld\n",pcount ,i+begin);
+            if (pcount<=100)
+                printf("%ld:  %ld\n",pcount ,i+begin);
             
         }
     }
-    printf("Total primes %ld\n", pcount);
 }
